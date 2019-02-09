d4b2068dcdd3234fdba367923a8c976cb19123e8
Romain Beaumont <romain.rom1@gmail.com>
2019-02-09 19:09:31 +0100

    fix js style

--- example/example-app.js
+++ example/example-app.js
@@ -8,18 +8,18 @@ class ExampleApp extends LitElement {
   render () {
     return html`
      <data-table .options=${{
-      'order': [[ 0, 'desc' ]],
-      'columns': [
-        { title: 'name' },
-        { title: 'position' },
-        { title: 'salary' },
-        { title: 'office' },
-        ],
-       'data': [
-         ['a1', 'b1', 'c1', 'd1'],
-         ['a2', 'b2', 'c2', 'd2']
-        ] 
-    }}></data-table>
+    'order': [[ 0, 'desc' ]],
+    'columns': [
+      { title: 'name' },
+      { title: 'position' },
+      { title: 'salary' },
+      { title: 'office' }
+    ],
+    'data': [
+      ['a1', 'b1', 'c1', 'd1'],
+      ['a2', 'b2', 'c2', 'd2']
+    ]
+  }}></data-table>
   `
   }
 }
